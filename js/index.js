2be1b3eb97c2268a7073852efe123f1ec2327ccd
KelvinMulyawan <kelvinmulyawan2000@gmail.com>
2021-10-15 14:38:03 +0700

    Adding JavaScript

--- js/index.js
+++ js/index.js
@@ -1,9 +1,22 @@
 function change(){
-    var fa = document.getElementById('buma');
+    var buma = document.getElementById('buma');
+    var bd = document.getElementById('body');
+    var navigator = document.getElementById('navbar');
 
     if (buma.className == 'fa fa-moon-o') {
         buma.className = 'fa fa-sun-o';
+        buma.style.color = "white";
+
+        navigator.style.color = "white";
+
+        bd.style.backgroundColor = "black";
+
     } else {
         buma.className = 'fa fa-moon-o';
+        buma.style.color = "black";
+
+        navigator.style.color = "black";
+        
+        bd.style.backgroundColor = "white";
     }
 }
