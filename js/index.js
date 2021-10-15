f5204f4407a5aa001794bf242740f9f49dea8dc4
KelvinMulyawan <kelvinmulyawan2000@gmail.com>
2021-10-15 14:31:17 +0700

    Adding JavaScript

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function change(){
+    var fa = document.getElementById('buma');
+
+    if (buma.className == 'fa fa-moon-o') {
+        buma.className = 'fa fa-sun-o';
+    } else {
+        buma.className = 'fa fa-moon-o';
+    }
+}
